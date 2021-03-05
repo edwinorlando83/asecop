ff00dac9bca18f920865e4e94b362181766588a8
Orlando <echolota@sweadenseguros.com>
2021-03-05 13:41:18 -0500

    arreglos en socios

--- asecop/asecop/doctype/socio/socio.js
+++ asecop/asecop/doctype/socio/socio.js
@@ -5,4 +5,22 @@ frappe.ui.form.on('Socio', {
 	// refresh: function(frm) {
 
 	// }
+	onload_post_render: function(frm) {
+
+		 
+			controlSoloNumeroEntero(frm.fields_dict.cedula);
+            controlSoloNumeroEntero(frm.fields_dict.cedulaconyuge);
+     
+    
+	  },
+	  validate: function (frm) {
+		if (validarCedula(frm.doc.cedula) == 0) {
+	
+		  frappe.throw("Cedula incorrecto del socio");
+		}
+		if (validarCedula(frm.doc.cedulaconyuge) == 0) {
+	
+			frappe.throw("Cedula incorrecto del conyuge");
+		  }
+	  }
 });
